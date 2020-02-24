081c7e3af383115e8cce8245861661586cb2b0bb
bordils <45363006+bordils@users.noreply.github.com>
2020-02-24 14:39:02 +0100

    added 3rd project

---
+++ 3_ArgonAssault/Assets/Splash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadnextScene", levelLoadDelay);
+    }
+
+    private void LoadnextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
