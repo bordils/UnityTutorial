e94a1dc023cb5204ffe524fce07c6cac61be9e40
bordils <45363006+bordils@users.noreply.github.com>
2020-02-28 14:27:19 +0100

    Added new game

---
+++ 4_RealmRush/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+    Waypoint waypoint;
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        transform.position = new Vector3 (  waypoint.GetGridPos().x,
+                                            0f, 
+                                            waypoint.GetGridPos().y);
+    }
+
+    private void UpdateLabel()
+    {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = waypoint.GetGridPos().x / waypoint.GetGridSize() + "." + waypoint.GetGridPos().y / waypoint.GetGridSize();
+        textMesh.text = labelText;
+        gameObject.name = "cube " + labelText;
+    }
+}
---
+++ 4_RealmRush/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float dwellTime = 1f;
+    [SerializeField] List<Waypoint> Path;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintAllWaypoints());
+    }
+
+    IEnumerator PrintAllWaypoints()
+    {
+        foreach(Waypoint block in Path)
+        {
+            transform.position = block.transform.position;
+            yield return new WaitForSeconds(dwellTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
